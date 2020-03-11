6ea53c7d53f8111b3c6217188f785c69cbb781be
Gordon <gordonmikkelsen87@gmail.com>
2020-03-11 14:49:19 +0100

    createt project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assignment_2_arraysorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
